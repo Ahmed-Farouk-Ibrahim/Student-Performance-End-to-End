04a9b355a25ce7832cc58f9d87184439b741dcc2
Ahmed Osman <engineer.ahmedfarouk@gmail.com>
2024-06-29 16:57:11 -0300

    create logger.py and exception.py

---
+++ src/logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from datetime import datetime
+
+# Generate a unique log file name based on the current datetime
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create the path to the logs directory
+logs_directory = os.path.join(os.getcwd(), "logs")
+
+# Ensure the logs directory exists
+try:
+    os.makedirs(logs_directory, exist_ok=True)
+except Exception as e:
+    print(f"Failed to create logs directory: {e}")
+    raise
+
+# Create the full path for the log file
+LOG_FILE_PATH = os.path.join(logs_directory, LOG_FILE)
+
+# Configure the logging settings
+logging.basicConfig(
+    filename=LOG_FILE_PATH,  # Set the log file path
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Set the log message format
+    level=logging.INFO,  # Set the default logging level to INFO
+)
+
+# demonstrate_logging customized function to check logging is working:
+def demonstrate_logging():
+    logging.info("This is an informational message")
+    logging.warning("This is a warning message")
+    logging.error("This is an error message")
+
+if __name__ == "__main__":
+    demonstrate_logging()
